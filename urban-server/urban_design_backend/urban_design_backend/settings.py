e0978cbbd4fd3de8e0deff0f9b048774ed0418e9
iota07 <hanesananhouvannasak@googlemail.com>
2024-04-30 15:22:10 +0200

    fixed cors - add sample login form

--- urban-server/urban_design_backend/urban_design_backend/settings.py
+++ urban-server/urban_design_backend/urban_design_backend/settings.py
@@ -46,6 +46,12 @@ INSTALLED_APPS = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
