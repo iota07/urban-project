2d87de0b642eb88f408e912d261b78481dd27981
iota07 <hanesananhouvannasak@googlemail.com>
2024-05-02 14:51:34 +0200

    added rest auth and endpoints

--- urban-server/urban_design_backend/urban_design_auth/urls.py
+++ urban-server/urban_design_backend/urban_design_auth/urls.py
@@ -1,9 +1,16 @@
 from . import views
 from django.urls import path
+from allauth.account.views import confirm_email
+from django.conf.urls import url
+from django.urls import path, include
 
 
 urlpatterns = [    
     path('home/', views.HomeView.as_view(), name ='home'),
     path('logout/', views.LogoutView.as_view(), name ='logout'),
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^account/', include('allauth.urls')),
+    url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
 ]
 
