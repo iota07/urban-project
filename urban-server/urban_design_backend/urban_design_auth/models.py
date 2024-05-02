a8c764ab559aefbb09f21400d89b474bc2ffaf4e
iota07 <hanesananhouvannasak@googlemail.com>
2024-05-02 11:40:02 +0200

    saving work

--- urban-server/urban_design_backend/urban_design_auth/models.py
+++ urban-server/urban_design_backend/urban_design_auth/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
+
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    organisation = models.CharField(max_length=100)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'organisation']
+
+    def __str__(self):
+        return self.email
