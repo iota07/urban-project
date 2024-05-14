30d3db73c4f9580bf9a65a0cebf17b03a12fc32e
iota07 <hanesananhouvannasak@googlemail.com>
2024-05-14 15:48:09 +0200

    added STL loader

--- urban-site/src/component/STLWithDataViewer.jsx
+++ urban-site/src/component/STLWithDataViewer.jsx
@@ -1,62 +1,91 @@
 import React, { useEffect, useRef } from "react";
-import "@kitware/vtk.js/Rendering/Profiles/Geometry";
-import vtkSTLReader from "vtk.js/Sources/IO/Geometry/STLReader";
-import vtkXMLPolyDataReader from "vtk.js/Sources/IO/XML/XMLPolyDataReader";
-import vtkFullScreenRenderWindow from "@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow";
+import * as THREE from "three";
+import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-import vtkActor from "@kitware/vtk.js/Rendering/Core/Actor";
-import vtkMapper from "@kitware/vtk.js/Rendering/Core/Mapper";
-import vtkConeSource from "@kitware/vtk.js/Filters/Sources/ConeSource";
-
-const STLWithDataViewer = ({ stlFile, vtkFile }) => {
+const STLWithDataViewer = ({ stlFile }) => {
   const containerRef = useRef();
 
   useEffect(() => {
-    const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
-      rootContainer: containerRef.current,
-      containerStyle: {},
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xcce0ff); // Light blue background
+
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    containerRef.current.appendChild(renderer.domElement);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.update();
+
+    const stlLoader = new STLLoader();
+
+    // Create a directional light to simulate sunlight
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(0, 1, 1).normalize(); // Position the light to shine from top to bottom
+    scene.add(light);
+
+    // Add an ambient light
+    const ambientLight = new THREE.AmbientLight(0x404040); // Soft ambient light
+    scene.add(ambientLight);
+
+    // Enable shadows in the renderer
+    renderer.shadowMap.enabled = true;
+
+    stlLoader.load(stlFile, function (geometry) {
+      geometry.computeBoundingBox();
+      const boundingBox = geometry.boundingBox;
+      const center = boundingBox.getCenter(new THREE.Vector3());
+      const size = boundingBox.getSize(new THREE.Vector3());
+
+      // Position camera to look at the center of the model
+      camera.position.copy(center);
+      camera.position.x += size.x;
+      camera.position.y += size.y;
+      camera.position.z += size.z;
+
+      const material = new THREE.MeshStandardMaterial({ color: 0xd3d3d3 }); // Use a standard material for the building
+      const mesh = new THREE.Mesh(geometry, material);
+
+      // Enable shadows for the mesh
+      mesh.castShadow = true;
+      mesh.receiveShadow = true;
+
+      // Adjust these values as needed to correct the orientation of the mesh
+      mesh.rotation.x = Math.PI * 0.45; // 180 degrees
+      mesh.rotation.y = Math.PI * 1.15;
+      mesh.rotation.z = Math.PI * 0.58;
+
+      scene.add(mesh);
+
+      // Make the camera look at the mesh
+      camera.lookAt(mesh.position);
+
+      // Set controls target to center of the model
+      controls.target.copy(mesh.position);
     });
-    const renderer = fullScreenRenderer.getRenderer();
-    const renderWindow = fullScreenRenderer.getRenderWindow();
 
-    const stlReader = vtkSTLReader.newInstance();
-    const vtkReader = vtkXMLPolyDataReader.newInstance();
-    const stlActor = vtkActor.newInstance();
-    const vtkActor = vtkActor.newInstance();
-    const stlMapper = vtkMapper.newInstance();
-    const vtkMapper = vtkMapper.newInstance();
+    const animate = function () {
+      requestAnimationFrame(animate);
+      controls.update();
+      renderer.render(scene, camera);
+    };
 
-    stlActor.setMapper(stlMapper);
-    vtkActor.setMapper(vtkMapper);
-
-    Promise.all([
-      stlReader.setUrl(stlFile, { binary: true }).catch((error) => {
-        console.error("Error loading STL file:", error);
-      }),
-      vtkReader.setUrl(vtkFile).catch((error) => {
-        console.error("Error loading VTK file:", error);
-      }),
-    ])
-      .then(() => {
-        stlMapper.setInputData(stlReader.getOutputData(0));
-        vtkMapper.setInputData(vtkReader.getOutputData(0));
-
-        renderer.addActor(stlActor);
-        renderer.addActor(vtkActor);
-
-        renderer.resetCamera();
-        renderWindow.render();
-      })
-      .catch((error) => {
-        console.error("Error loading one or more files:", error);
-      });
+    animate();
 
     return () => {
-      fullScreenRenderer.delete();
+      while (containerRef.current.firstChild) {
+        containerRef.current.firstChild.remove();
+      }
     };
-  }, [stlFile, vtkFile]);
+  }, [stlFile]);
 
-  return <div ref={containerRef} style={{ width: "100%", height: "100%" }} />;
+  return <div ref={containerRef} />;
 };
 
 export default STLWithDataViewer;
--- urban-site/src/routes/Landingpage/Landingpage.jsx
+++ urban-site/src/routes/Landingpage/Landingpage.jsx
@@ -5,14 +5,9 @@ import CFD_data_z5 from "../../assets/CFD_data_z5.vtk";
 
 function Landingpage() {
   return (
-    <section className="flex items-center justify-center h-screen bg-blue-300 bg-opacity-50">
-      <div className="p-6 bg-white bg-opacity-75 rounded shadow-xl">
-        <h1 className="text-4xl font-bold text-blue-500">
-          Welcome to the landing page!
-        </h1>
-        <STLWithDataViewer stlFile={buildings} vtkFile={CFD_data_z5} />
-      </div>
-    </section>
+    <>
+      <STLWithDataViewer stlFile={buildings} vtkFile={CFD_data_z5} />
+    </>
   );
 }
 
