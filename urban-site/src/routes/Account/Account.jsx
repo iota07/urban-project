5956381bbc391b8ff4d8039b0d7e4fd1c2f6eb0a
iota07 <hanesananhouvannasak@googlemail.com>
2024-06-03 14:45:27 +0200

    fetch user data and display as initial value in account input field

--- urban-site/src/routes/Account/Account.jsx
+++ urban-site/src/routes/Account/Account.jsx
@@ -2,8 +2,9 @@ import axios from "axios";
 import React from "react";
 import { Formik, Form, useField } from "formik";
 import * as Yup from "yup";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FiEye, FiEyeOff, FiInfo } from "react-icons/fi";
+import TitleH2 from "../../component/TitleH2";
 
 const MyTextInput = ({ label, ...props }) => {
   const [field, meta] = useField(props);
@@ -16,95 +17,47 @@ const MyTextInput = ({ label, ...props }) => {
     name: "Name can only contain alphanumeric characters. Spaces, '<', and '>' are not allowed",
     surname:
       "Surname can only contain alphanumeric characters. Spaces, '<', and '>' are not allowed",
-  };
-
-  return (
-    <>
-      <div>
-        <div className="group relative">
-          {validationMessages[props.name] && (
-            <div className="absolute left-0 top-0 transform -translate-x-full translate-y-1/2 cursor-pointer">
-              <FiInfo
-                className="text-white"
-                onMouseEnter={() => setShowTooltip(true)}
-                onMouseLeave={() => setShowTooltip(false)}
-              />
-              {showTooltip && (
-                <div className="absolute left-80 -top-14 text-sm transform -translate-x-full w-64 p-2 bg-white border rounded-xl shadow-md z-50">
-                  {validationMessages[props.name]}
-                </div>
-              )}
-            </div>
-          )}
-          <input
-            {...field}
-            {...props}
-            id={props.name}
-            required
-            className="peer m-0 p-0 h-14 w-full rounded-3xl bg-gray-100 px-4 text-sm outline-none"
-          />
-          <label
-            htmlFor={props.name}
-            className="absolute left-2 top-0 flex h-full transform items-center pl-2 text-base transition-all duration-300 group-focus-within:-top-7 group-focus-within:h-1/2 group-focus-within:pl-0 group-focus-within:text-base group-focus-within:text-white peer-valid:-top-7 peer-valid:h-1/2 peer-valid:pl-0 peer-valid:text-base peer-valid:text-white"
-          >
-            {label}
-          </label>
-        </div>
-        {meta.touched && meta.error ? (
-          <p className="max-w-sm text-red-500 pb-2">{meta.error}</p>
-        ) : null}
-      </div>
-    </>
-  );
-};
-
-const MyOptionalTextInput = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
-  const [showTooltip, setShowTooltip] = useState(false);
-
-  const validationMessages = {
     organisation: "Organisation cannot contain '<' or '>'",
   };
 
   return (
     <>
-      <div>
-        <div className="group relative">
-          {validationMessages[props.name] && (
-            <div className="absolute left-0 top-0 transform -translate-x-full translate-y-1/2 cursor-pointer">
-              <FiInfo
-                className="text-white"
-                onMouseEnter={() => setShowTooltip(true)}
-                onMouseLeave={() => setShowTooltip(false)}
-              />
-              {showTooltip && (
-                <div className="absolute left-80 -top-14 text-sm transform -translate-x-full w-64 p-2 bg-white border rounded-xl shadow-md z-50">
-                  {validationMessages[props.name]}
-                </div>
-              )}
-            </div>
-          )}
+      <section>
+        <div className="group relative flex flex-col place-items-start">
+          <div>
+            {validationMessages[props.name] && (
+              <div className="absolute left-0 top-0 transform -translate-x-full translate-y-1/2 cursor-pointer">
+                <FiInfo
+                  className="text-white"
+                  onMouseEnter={() => setShowTooltip(true)}
+                  onMouseLeave={() => setShowTooltip(false)}
+                />
+                {showTooltip && (
+                  <div className="absolute left-80 -top-14 text-sm transform -translate-x-full w-64 p-2 bg-white border rounded-xl shadow-md z-50">
+                    {validationMessages[props.name]}
+                  </div>
+                )}
+              </div>
+            )}
+            <label
+              htmlFor={props.name}
+              className="text-lg text-primary ml-3 mb-1"
+            >
+              {label}
+            </label>
+          </div>
           <input
             {...field}
             {...props}
             id={props.name}
-            className={`peer m-0 p-0 h-14 w-full rounded-3xl bg-gray-100 px-4 text-sm outline-none ${
-              field.value ? "optional-text" : ""
-            }`}
+            className="peer m-0 p-0 h-14 w-full rounded-3xl bg-gray-100 px-4 text-sm outline-none"
+            placeholder={props.placeholder}
           />
-          <label
-            htmlFor={props.name}
-            className={`absolute left-2 top-0 flex h-full transform items-center pl-2 text-base transition-all duration-300 ${
-              field.value
-                ? "-top-7 h-1/2 pl-0 text-base text-white"
-                : "peer-focus:-top-7 peer-focus:h-1/2 peer-focus:pl-0 peer-focus:text-base peer-focus:text-white"
-            }`}
-          >
-            {label}
-          </label>
         </div>
-        {meta.error && <div className="error">{meta.error}</div>}
-      </div>
+        {meta.touched && meta.error ? (
+          <p className="max-w-sm text-red-500 pb-2">{meta.error}</p>
+        ) : null}
+      </section>
     </>
   );
 };
@@ -122,7 +75,7 @@ const MyPasswordInput = ({ label, ...props }) => {
   return (
     <>
       <div>
-        <div className="group relative">
+        <div className="group relative flex flex-col place-items-start">
           {validationMessages[props.name] && (
             <div className="absolute left-0 top-0 transform -translate-x-full translate-y-1/2 cursor-pointer">
               <FiInfo
@@ -137,22 +90,21 @@ const MyPasswordInput = ({ label, ...props }) => {
               )}
             </div>
           )}
+          <label
+            htmlFor={props.name}
+            className="text-lg text-primary ml-3 mb-1"
+          >
+            {label}
+          </label>
           <input
             {...field}
             {...props}
             id={props.name}
-            required
             type={showPassword ? "text" : "password"}
             className="peer m-0 p-0 h-14 w-full rounded-3xl bg-gray-100 px-4 text-sm outline-none"
           />
-          <label
-            htmlFor={props.name}
-            className="absolute left-2 top-0 flex h-full transform items-center pl-2 text-base transition-all duration-300 group-focus-within:-top-7 group-focus-within:h-1/2 group-focus-within:pl-0 group-focus-within:text-base group-focus-within:text-white peer-valid:-top-7 peer-valid:h-1/2 peer-valid:pl-0 peer-valid:text-base peer-valid:text-white"
-          >
-            {label}
-          </label>
           <span
-            className="absolute right-2 sm:right-4 top-1/2 transform -translate-y-1/2 cursor-pointer"
+            className="absolute right-5 top-14 transform -translate-y-1/2 cursor-pointer"
             onClick={() => setShowPassword((prev) => !prev)}
           >
             {showPassword ? <FiEyeOff /> : <FiEye />}
@@ -169,43 +121,65 @@ const MyPasswordInput = ({ label, ...props }) => {
 const validationSchema = Yup.object().shape({
   email: Yup.string()
     .email("Invalid email")
-    .matches(/^[^\s<>]*$/, "Email cannot contain spaces, '<', or '>'")
-    .required("Required"),
+    .matches(/^[^\s<>]*$/, "Email cannot contain spaces, '<', or '>'"),
+
   username: Yup.string()
     .matches(
       /^[a-zA-Z0-9_-]*$/,
       "Username can only contain alphanumeric characters, hyphens, and underscores"
     )
-    .matches(/^[^\s<>]*$/, "Username cannot contain spaces, '<', or '>'")
-    .required("Required"),
+    .matches(/^[^\s<>]*$/, "Username cannot contain spaces, '<', or '>'"),
+
   password1: Yup.string()
     .min(8, "Must be at least 8 characters")
-    .matches(/^[^\s<>]*$/, "Password cannot contain spaces, '<', or '>'")
-    .required("Required"),
+    .matches(/^[^\s<>]*$/, "Password cannot contain spaces, '<', or '>'"),
+
   password2: Yup.string()
     .oneOf([Yup.ref("password1"), null], "Passwords must match")
     .matches(
       /^[^\s<>]*$/,
       "Password confirmation cannot contain spaces, '<', or '>'"
-    )
-    .required("Required"),
+    ),
+
   name: Yup.string()
     .matches(/^[a-zA-Z0-9]*$/, "Name can only contain alphanumeric characters")
-    .matches(/^[^\s<>]*$/, "Name cannot contain spaces, '<', or '>'")
-    .required("Required"),
+    .matches(/^[^\s<>]*$/, "Name cannot contain spaces, '<', or '>'"),
+
   surname: Yup.string()
     .matches(
       /^[a-zA-Z0-9]*$/,
       "Surname can only contain alphanumeric characters"
     )
-    .matches(/^[^\s<>]*$/, "Surname cannot contain spaces, '<', or '>'")
-    .required("Required"),
-  organisation: Yup.string()
-    .matches(/^[^\s<>]*$/, "Organisation cannot contain spaces, '<', or '>'")
-    .notRequired(),
+    .matches(/^[^\s<>]*$/, "Surname cannot contain spaces, '<', or '>'"),
+
+  organisation: Yup.string().matches(
+    /^[^\s<>]*$/,
+    "Organisation cannot contain spaces, '<', or '>'"
+  ),
 });
 
 const Account = () => {
+  const [userData, setUserData] = useState(null);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8000/dj-rest-auth/user/"
+        );
+
+        if (response.data) {
+          console.log("Fetched user data:", response.data);
+          const { email, username, name, surname, organisation } =
+            response.data;
+          setUserData({ email, username, name, surname, organisation });
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
   const submit = async (values, { setFieldError }) => {
     const user = {
       email: values.email,
@@ -219,7 +193,7 @@ const Account = () => {
 
     try {
       await axios
-        .post("http://localhost:8000/dj-rest-auth/registration/", user, {
+        .patch("http://localhost:8000/dj-rest-auth/user/", user, {
           headers: {
             "Content-Type": "application/json",
           },
@@ -252,41 +226,58 @@ const Account = () => {
         <div className="w-[400px] xl:w-[500px] rounded-3xl bg-secondary flex justify-center items-start">
           <div className="flex h-auto w-full flex-col pb-16 mb-2 mt-4 sm:mt-8 lg:mt-12 rounded-3xl bg-white bg-opacity-20">
             <Formik
-              initialValues={{
-                email: "",
-                username: "",
-                password1: "",
-                password2: "",
-                name: "",
-                surname: "",
-                organisation: "",
-              }}
+              initialValues={
+                userData || {
+                  email: "",
+                  username: "",
+                  name: "",
+                  surname: "",
+                  organisation: "",
+                }
+              }
+              enableReinitialize={true}
               validationSchema={validationSchema}
               onSubmit={submit}
             >
               {({ errors, touched }) => (
                 <Form>
                   <fieldset className="flex flex-col gap-6 mt-10 px-10 py-10 text-center">
-                    <MyTextInput name="email" type="text" label="Email" />
-                    <MyTextInput name="username" type="text" label="Username" />
-                    <MyTextInput name="name" type="text" label="Name" />
-                    <MyTextInput name="surname" type="text" label="Surname" />
-                    <MyOptionalTextInput
+                    <TitleH2 title="Account" />
+                    <MyTextInput
+                      name="email"
+                      type="text"
+                      label="Email"
+                      placeholder={userData ? userData.email : ""}
+                    />
+                    <MyTextInput
+                      name="username"
+                      type="text"
+                      label="Username"
+                      placeholder={userData ? userData.username : ""}
+                    />
+                    <MyTextInput
+                      name="name"
+                      type="text"
+                      label="Name"
+                      placeholder={userData ? userData.name : ""}
+                    />
+                    <MyTextInput
+                      name="surname"
+                      type="text"
+                      label="Surname"
+                      placeholder={userData ? userData.surname : ""}
+                    />
+                    <MyTextInput
                       name="organisation"
                       type="text"
-                      label={
-                        <>
-                          <span>Organisation</span>{" "}
-                          <span className="pl-2 text-gray-300">(optional)</span>
-                        </>
-                      }
+                      label="Organisation"
+                      placeholder={userData ? userData.organisation : ""}
                     />
-                    <MyPasswordInput name="password1" label="Password" />
+                    <MyPasswordInput name="password1" label="New Password" />
                     <MyPasswordInput
                       name="password2"
-                      label="Password confirmation"
+                      label="New Password confirmation"
                     />
-
                     <button
                       type="submit"
                       className="mt-4 h-12 w-full rounded-3xl bg-primary text-white transition-all duration-300 hover:bg-success"
