f9af49c39b05518841ec52e436dd9efba21c669c
iota07 <hanesananhouvannasak@googlemail.com>
2024-06-10 13:22:39 +0200

    add allowed host to vercel app

--- urban-server/urban_design_backend/urban_design_backend/settings.py
+++ urban-server/urban_design_backend/urban_design_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['urban-project.vercel.app']
 
 
 # Application definition
@@ -59,6 +59,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://urban-project.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
