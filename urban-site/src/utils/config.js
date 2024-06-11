9467c3bb825af2f90e26b22f0219d75ee1a5a352
iota07 <hanesananhouvannasak@googlemail.com>
2024-06-11 17:32:31 +0200

    remove hardcoded config.js

--- urban-site/src/utils/config.js
+++ urban-site/src/utils/config.js
@@ -1,1 +1,3 @@
-export const BACKEND_URL = "https://urban-project.onrender.com";
+// Access the environment variable
+export const BACKEND_URL =
+  import.meta.env.VITE_APP_BACKEND_URL || "https://default-url.com";
