57aeca6b674d1d51fdd65a8614a743f2f6986ae9
iota07 <hanesananhouvannasak@googlemail.com>
2024-06-13 16:02:36 +0200

    move allauth at bottom

--- urban-server/urban_design_backend/urban_design_auth/urls.py
+++ urban-server/urban_design_backend/urban_design_auth/urls.py
@@ -13,13 +13,12 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name ='logout'),
     path('user/', UserDetailView.as_view(), name='user_detail'),
     path('user/password/', UpdatePasswordView.as_view(), name='update_password'),
-    path('delete_account/', DeleteAccountView.as_view(), name='delete_account'),  
-    
+    path('delete_account/', DeleteAccountView.as_view(), name='delete_account'),      
     re_path(r'^dj-rest-auth/registration/$', CustomRegisterView.as_view(), name='account_signup'),    
     re_path(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
-    path('dj-rest-auth/password/reset/confirm/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    re_path(r'^accounts/', include('allauth.urls')),
+    path('dj-rest-auth/password/reset/confirm/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),    
     re_path(r'^dj-rest-auth/', include('dj_rest_auth.urls')),
+    re_path(r'^accounts/', include('allauth.urls')),
     
 ]
 
