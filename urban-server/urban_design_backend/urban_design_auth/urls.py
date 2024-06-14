e7243b81b342280a2d0adeb6f35c47515057b66b
iota07 <hanesananhouvannasak@googlemail.com>
2024-06-14 09:56:58 +0200

    revert url

--- urban-server/urban_design_backend/urban_design_auth/urls.py
+++ urban-server/urban_design_backend/urban_design_auth/urls.py
@@ -4,8 +4,6 @@ from django.urls import path, re_path, include
 from .views import UserDetailView, UpdatePasswordView, DeleteAccountView
 from urban_design_auth.views import CustomPasswordResetConfirmView
 from allauth.account.views import confirm_email
-from rest_framework_simplejwt.views import TokenRefreshView
-from urban_design_auth.views import CustomTokenObtainPairView
 
 
 
@@ -15,14 +13,12 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name ='logout'),
     path('user/', UserDetailView.as_view(), name='user_detail'),
     path('user/password/', UpdatePasswordView.as_view(), name='update_password'),
-    path('delete_account/', DeleteAccountView.as_view(), name='delete_account'),  
-    path('token/login/', CustomTokenObtainPairView.as_view(), name='token_login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name ='token_refresh'),    
+    path('delete_account/', DeleteAccountView.as_view(), name='delete_account'),        
     re_path(r'^dj-rest-auth/registration/$', CustomRegisterView.as_view(), name='account_signup'),    
     re_path(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
     path('dj-rest-auth/password/reset/confirm/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),    
     re_path(r'^dj-rest-auth/', include('dj_rest_auth.urls')),
-    re_path(r'^accounts/', include('allauth.urls')),
+    
     
 ]
 
