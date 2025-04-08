739be91ff837404a7eb6f89ec06c273e93f7832d
iota07 <hanesananhouvannasak@googlemail.com>
2025-04-08 17:55:45 +0200

    remove title1 selection

--- urban-site/src/component/TitleH1.jsx
+++ urban-site/src/component/TitleH1.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function TitleH1(props) {
   return (
     <section className="text-center">
-      <h1 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-8xl tracking-tight font-bold text-primary">
+      <h1 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-8xl tracking-tight font-bold text-primary select-none">
         {props.title}
       </h1>
     </section>
